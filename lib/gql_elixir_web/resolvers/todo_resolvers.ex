b749bdf1a92ffec3cef129f08d27389db97acb13
Lucas Stettner <lebra448@gmail.com>
2020-04-21 10:14:31 -0400

    add todo types and resolvers

---
+++ lib/gql_elixir_web/resolvers/todo_resolvers.ex
@@ -0,0 +1,29 @@
+defmodule GqlElixirWeb.Resolvers.TodoResolver do
+  alias GqlElixir.Todo
+
+  def list_items(_parent, _args, _resolutions) do
+    {:ok, Todo.list_items()}
+  end
+
+  def create_item(_parent, args, _resolutions) do
+    args
+    |> Todo.create_item()
+    |> case do
+      {:ok, user} ->
+        {:ok, user}
+
+      {:error, changeset} ->
+        {:error, extract_error_msg(changeset)}
+    end
+  end
+
+  defp extract_error_msg(changeset) do
+    changeset.errors
+    |> Enum.map(fn {field, {error, _details}} ->
+      [
+        field: field,
+        message: String.capitalize(error)
+      ]
+    end)
+  end
+end
---
+++ lib/gql_elixir_web/schema/todo_types.ex
@@ -0,0 +1,29 @@
+defmodule GqlElixirWeb.Schema.TodoTypes do
+  use Absinthe.Schema.Notation
+  use Absinthe.Ecto, repo: GqlElixir.Repo
+
+  alias GqlElixirWeb.Resolvers
+
+  @desc "One todo item"
+  object :item do
+    field :id, :id
+    field :title, :string
+    field :checked, :boolean
+  end
+
+  object :item_queries do
+    @desc "Get all todo items"
+    field :list_items, list_of(:item) do
+      resolve(&Resolvers.TodoResolver.list_items/3)
+    end
+  end
+
+  object :item_mutations do
+    field :create_item, :item do
+      arg(:title, non_null(:string))
+      arg(:checked, :boolean)
+
+      resolve(&Resolvers.TodoResolver.create_item/3)
+    end
+  end
+end
